43fa1dafb358b9b9e19704cd7cee93a40ff9e10d
Francis1240 <31699006+Francis1240@users.noreply.github.com>
2020-10-21 23:06:06 -0500

    2

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import matplotlib.pyplot as plt
+import math
+import csv
 import networkx as nx
 import os
+from scipy.optimize import curve_fit
 GSeq = nx.MultiDiGraph()
 GSen = nx.MultiGraph()
 try:
@@ -33,3 +37,47 @@ for line in news_lines:
 nx.write_gexf(GSen, "sen.gexf", encoding='utf-8')
 
 news_file.close()
+
+with open('sen.csv', "r", newline='', encoding='utf-8') as sen_file:
+    reader = csv.reader(sen_file, delimiter=',', quotechar='|')
+    count = {0: 0}
+    x = []
+    y = []
+    for row in reader:
+        deg = math.floor(float(row[5]))
+        count[deg] = count.get(deg, 0) + 1
+    for k in count:
+        x.append(k)
+        y.append(count[k])
+
+    plt.scatter(x, y, color="blue")
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.ylim(1, 40)
+    plt.xlim(1, 13000)
+    plt.title("Sentence Network")
+    plt.xlabel("Weighted Degree")
+    plt.ylabel("Count")
+    plt.show()
+
+with open('seq.csv', "r", newline='', encoding='utf-8') as seq_file:
+    reader = csv.reader(seq_file, delimiter=',', quotechar='|')
+    count = {0: 0}
+    x = []
+    y = []
+    for row in reader:
+        deg = math.floor(float(row[10]))
+        count[deg] = count.get(deg, 0) + 1
+    for k in count:
+        x.append(k)
+        y.append(count[k])
+
+    plt.scatter(x, y, color="blue")
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.ylim(1, 500)
+    plt.xlim(1, 1000)
+    plt.title("Sequential Network")
+    plt.xlabel("Weighted Degree")
+    plt.ylabel("Count")
+    plt.show()
